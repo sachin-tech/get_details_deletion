83e82ac88393c185ce298c073bc3672936d28679
sachin-tech <51818996+sachin-tech@users.noreply.github.com>
2022-09-15 19:53:19 +0530

    Create details_deletion.py

---
+++ details_deletion.py
@@ -0,0 +1,12 @@
+import boto3
+#ec2 = boto3.client('ec2')
+#myinstance = ec2.describe_instances()
+ec2 = boto3.resource('ec2')
+base = ec2.instances.filter()
+filters = [{
+    'Name': 'Name',
+    'Values': ['Jenkins_Built_instance']
+}]
+filtered1 = base.filter(Filters=filters)
+for instance in filtered1:
+  print(instance.id)
