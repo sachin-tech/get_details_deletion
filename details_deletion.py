0e506d00299447f9f87a2bdf4135dacc4f4d8379
sachin-tech <51818996+sachin-tech@users.noreply.github.com>
2022-09-15 20:15:08 +0530

    Update details_deletion.py

--- details_deletion.py
+++ details_deletion.py
@@ -2,7 +2,7 @@ import boto3
 #ec2 = boto3.client('ec2')
 #myinstance = ec2.describe_instances()
 ec2 = boto3.resource('ec2')
-instances = ec2.instances.filter(Filters=[{'Name': 'instance-state-name', 'Values': ['running']}])
+instances = ec2.instances.filter(Filters=[{'Name': 'instance-name', 'Values': ['Jenkins_Built_instance']}])
 for instance in instances:
     if instance.tags == None:
         print(instance.id)
