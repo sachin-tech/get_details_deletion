c70a198e9bac6b767ceee326c10d44be025a39cc
sachin-tech <51818996+sachin-tech@users.noreply.github.com>
2022-09-15 20:39:07 +0530

    Update details_deletion.py

--- details_deletion.py
+++ details_deletion.py
@@ -3,7 +3,8 @@ import boto3
 #myinstance = ec2.describe_instances()
 ec2 = boto3.resource('ec2')
 instances = ec2.instances.filter(Filters=[{'Name': 'tag:Name', 'Values': ['jenkins_server']}, {'Name': 'instance-state-name', 'Values': ['running']}])
-instances_to_delete = [to_del for to_del in instances if to_del.id not in [i.id for i in instances]]  
-print("stopping instance") 
-instance.stop()
+instances_to_delete = [to_del for to_del in instances if to_del.id not in [i.id for i in instances]]
+for instance in instances_to_delete:
+ print("stopping instance") 
+ instance.stop()
   
