17bb9ce0bfd2f8a7c0584815d6efe9c45aebe10d
sachin-tech <51818996+sachin-tech@users.noreply.github.com>
2022-09-15 21:22:13 +0530

    Update details_deletion.py

--- details_deletion.py
+++ details_deletion.py
@@ -3,7 +3,7 @@ TAGS = ['production', 'development']
 #ec2 = boto3.client('ec2')
 #myinstance = ec2.describe_instances()
 ec2 = boto3.resource('ec2')
-instances = ec2.instances.filter(Filters=[{'Name': 'tag:Purpose', 'Values': ['Production']}, {'Name': 'tag:Purpose', 'Values': ['Dev']}])
+instances = ec2.instances.filter(Filters=[{'Name': 'tag:Purpose', 'Values': ['Production']}])
 for instance in instances:
    id=instance.id
    ec2.instances.filter(InstanceIds=[id]).stop()
