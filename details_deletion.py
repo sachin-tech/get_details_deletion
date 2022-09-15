76da1e00145129f5fcfe8ac3ed6cd94133df90ab
sachin-tech <51818996+sachin-tech@users.noreply.github.com>
2022-09-15 21:18:56 +0530

    Update details_deletion.py

--- details_deletion.py
+++ details_deletion.py
@@ -3,11 +3,8 @@ TAGS = ['production', 'development']
 #ec2 = boto3.client('ec2')
 #myinstance = ec2.describe_instances()
 ec2 = boto3.resource('ec2')
-instances = ec2.instances.filter()
-tags = {}
-for tag in instances:
- tags[tag['Key']] = tag['Value']
- if('Purpose' not in tags or tags['Purpose'] not in TAGS):
+instances = ec2.instances.filter(Filters=[{'Name': 'tag:Purpose', 'Values': ['Production']}, {'Name': 'tag:Purpose', 'Values': ['Dev']}])
+if instance not in instances:
    instances.stop()
 
   
