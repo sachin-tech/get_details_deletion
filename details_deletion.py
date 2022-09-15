2dfda30db7aed2e90f757a8391981218eb4dd926
sachin-tech <51818996+sachin-tech@users.noreply.github.com>
2022-09-15 21:07:50 +0530

    Update details_deletion.py

--- details_deletion.py
+++ details_deletion.py
@@ -1,10 +1,11 @@
 import boto3
+TAGS = ['production', 'development']
 #ec2 = boto3.client('ec2')
 #myinstance = ec2.describe_instances()
 ec2 = boto3.resource('ec2')
 instances = ec2.instances.filter()
 tags = {}
-for tag in instances.tags:
+for tag in instances:
  tags[tag['Key']] = tag['Value']
  if('Purpose' not in tags or tags['Purpose'] not in TAGS):
    instances.stop()
