8a591fff5500146efbe7ad531b9e43f6415c2945
sachin-tech <51818996+sachin-tech@users.noreply.github.com>
2022-09-15 21:02:11 +0530

    Update details_deletion.py

--- details_deletion.py
+++ details_deletion.py
@@ -3,7 +3,7 @@ import boto3
 #myinstance = ec2.describe_instances()
 ec2 = boto3.resource('ec2')
 #instances = ec2.instances.filter(Filters=[{'Name': 'tag:Name', 'Values': ['jenkins_server']}, {'Name': 'instance-state-name', 'Values': ['running']}])
-instance_object = ec2.Instance(instance['instanceId'])
+instance_object = ec2.Instance()
 tags = {}
 for tag in instance_object.tags:
  tags[tag['Key']] = tag['Value']
