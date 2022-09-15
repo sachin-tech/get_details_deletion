30ef41521b46f2d879e5ae94f2e14b2dc74a88eb
sachin-tech <51818996+sachin-tech@users.noreply.github.com>
2022-09-15 20:13:17 +0530

    Update details_deletion.py

--- details_deletion.py
+++ details_deletion.py
@@ -2,7 +2,7 @@ import boto3
 #ec2 = boto3.client('ec2')
 #myinstance = ec2.describe_instances()
 ec2 = boto3.resource('ec2')
-instances = ec2.instances.filter(Filters=[{'Name': 'instance-state-name', 'Values': ['stopped']}])
+instances = ec2.instances.filter(Filters=[{'Name': 'instance-state-name', 'Values': ['running']}])
 for instance in instances:
     if instance.tags == None:
         print(instance.id)
